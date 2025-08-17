d6c8413a7f5e98194bfb39b5fc4a66ae502c141d
GuilhermeVieiraf <guilhermevfelix06@gmail.com>
2025-08-17 12:07:35 -0300

    feat: tela de carregamento pro site

---
+++ public/assets/js/components/preloader.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function () {
+
+    const preloader = document.getElementById('preloader');
+
+
+    if (preloader) {
+
+        preloader.classList.add('hidden');
+    }
+});
